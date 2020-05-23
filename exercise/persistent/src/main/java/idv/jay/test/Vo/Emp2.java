3fee77dad1cd97204a5087de1397c25614cff01d
jay <a0970532713@gmail.com>
2020-05-23 18:51:50 +0800

    mybatis學習 20200523

---
+++ exercise/persistent/src/main/java/idv/jay/test/Vo/Emp2.java
@@ -0,0 +1,91 @@
+package idv.jay.test.Vo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ * 練習用員工vo
+ * @author User
+ * date 2020-5-23
+ */
+public class Emp2 implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int empno;
+	private String ename;
+	private String job;
+	private Date hiredate;
+	private double sal;
+	private double comm;
+	private int deptno;
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	public double getComm() {
+		return comm;
+	}
+	public void setComm(double comm) {
+		this.comm = comm;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	@Override
+	public String toString() {
+		return "emp2 [empno=" + empno + ", ename=" + ename + ", job=" + job + ", hiredate=" + hiredate + ", sal=" + sal
+				+ ", comm=" + comm + ", deptno=" + deptno + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + empno;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Emp2 other = (Emp2) obj;
+		if (empno != other.empno)
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ exercise/persistent/src/test/java/testMybatisDAO/TestEmp2DAO.java
@@ -0,0 +1,99 @@
+package testMybatisDAO;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import idv.jay.test.Vo.Emp2;
+
+/**
+ * 測試emp2 DAO method
+ * @author User
+ * date 2020-05-23
+ */
+public class TestEmp2DAO {
+
+	private final String RESOURCE_NAME = "SqlMapConfig.xml";
+	
+	/**
+	 * 根據員工編號查詢員工
+	 */
+	@Test
+	public void findEMP2ByIdTest() {
+//		創建session工廠
+		SqlSessionFactory sqlSessionFactory = null;
+		try {
+			sqlSessionFactory = 
+					new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(RESOURCE_NAME));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+//		通過SqlSessionFactory取得SqlSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		/*
+		 * 通過sqlSession跟資料庫進行溝通
+		 * 第一個參數:mapper.xml中statementId，等於namespace.statementId
+		 * 第二個參數:放入和mapper.xml中和parameterType匹配的參數
+		 * selectOne回傳結果是mapper.xml中resultType的物件
+		 */
+		Emp2 emp = sqlSession.selectOne("testMybatis.EMP2.findEMP2ById", 7001);
+		System.out.println(emp);
+		
+//		須關閉資源
+		sqlSession.close();
+	}
+	
+	/**
+	 * 根據員工姓名模糊查詢員工
+	 */
+	@Test
+	public void findEMP2ByName() {
+		SqlSessionFactory sqlSessionFactory = null;
+		try {
+			sqlSessionFactory = 
+					new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(RESOURCE_NAME));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		java.util.List<Emp2> emps = sqlSession.selectList("testMybatis.EMP2.findEMP2ByName", "S");
+		System.out.println(emps);
+		sqlSession.close();
+	}
+	
+	/**
+	 * 新增員工
+	 */
+	@Test
+	public void insertEmp2() {
+		SqlSessionFactory sqlSessionFactory = null;
+		try {
+			sqlSessionFactory = 
+					new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(RESOURCE_NAME));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Emp2 emp = new Emp2();
+		emp.setEmpno(7015);
+		emp.setEname("jay");
+		emp.setComm(100.4);
+		emp.setJob("工程師");
+		emp.setSal(100.5);
+		emp.setHiredate(Date.valueOf("2020-05-23"));
+		emp.setDeptno(10);
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		sqlSession.insert("testMybatis.EMP2.insertEmp2", emp);
+		
+//		需要自己commit， 預設autoCommit = false
+		sqlSession.commit();
+		sqlSession.close();
+	}
+}
